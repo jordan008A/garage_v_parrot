07190a74ff507d2959cf27707af37e6f8e5e98ae
jordan008A <jordancros@hotmail.fr>
2024-01-09 10:55:58 +0100

    🚧 Update Domain name

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -21,7 +21,7 @@ class MailerService
             ->from('garageparrot43@outlook.fr')
             ->to($user->getEmail())
             ->subject('Réinitialisation de votre mot de passe')
-            ->html("Pour réinitialiser votre mot de passe, veuillez cliquer sur ce lien: <a href='http://yourdomain.com/reset-password?token=$resetToken'>Réinitialiser le mot de passe</a>. Attention, ce lien restera actif 1 heure.");
+            ->html("Pour réinitialiser votre mot de passe, veuillez cliquer sur ce lien: <a href='https://garage-vincent-parrot-studi-d66e05141e08.herokuapp.com//reset-password?token=$resetToken'>Réinitialiser le mot de passe</a>. Attention, ce lien restera actif 1 heure.");
 
         $this->mailer->send($email);
     }
